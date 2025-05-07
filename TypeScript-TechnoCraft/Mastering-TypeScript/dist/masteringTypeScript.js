73769b89ff5ad60db89aec724b39812fcb7eb5d3
morsalin-islam335 <weshallovercome335@gmail.com>
2025-05-08 02:39:43 +0600

    modified masteringTypeScript.ts file, works with encapsulation

--- TypeScript-TechnoCraft/Mastering-TypeScript/dist/masteringTypeScript.js
+++ TypeScript-TechnoCraft/Mastering-TypeScript/dist/masteringTypeScript.js
@@ -6,4 +6,50 @@ function formatString(input, toUpper) {
     else
         return input.toLocaleLowerCase();
 }
-console.log(formatString("hello", false));
+// console.log(formatString("Hello",)); // it output will be HELLO
+// console.log(formatString("Hello",true)); // it output will be HELLO
+// console.log(formatString("Hello", false)); // it output will be hello
+// problem 2
+function filterByRating(items) {
+    const itemsWithHighestRating = items.filter(item => (item.rating >= 4));
+    return itemsWithHighestRating;
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+//   console.log(filterByRating(books)); // output will be [ { title: 'Book A', rating: 4.5 }, { title: 'Book C', rating: 5 } ]
+// problem 3
+function concatenateArrays(...arrays) {
+    var primaryArray = [];
+    arrays.forEach(array => {
+        primaryArray = primaryArray.concat(array);
+    });
+    return primaryArray;
+}
+//   console.log(concatenateArrays<string>(["a", "b"], ["c"])); // output will be [ 'a', 'b', 'c' ]
+//   console.log(concatenateArrays<number>([10, 20], [30])); // output will be [10, 20, 30]
+// problem 4
+class Vehicle {
+    // make constructor to assign private property
+    constructor(make, year) {
+        this._make = make;
+        this._year = year;
+    }
+    getInfo() {
+        console.log(`Make: ${this._make}, Year: ${this._year}`);
+    }
+}
+class Car extends Vehicle {
+    constructor(make, year, model) {
+        super(make, year);
+        this._model = model;
+    }
+    getModel() {
+        console.log(`Model: ${this._model}`);
+    }
+}
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();
+myCar.getModel();
