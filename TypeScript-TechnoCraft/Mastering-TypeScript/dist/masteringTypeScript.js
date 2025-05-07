a12cabc573fabb7cccd3e568a5bd3300a02fac31
morsalin-islam335 <weshallovercome335@gmail.com>
2025-05-08 03:37:14 +0600

    working with enum object

--- TypeScript-TechnoCraft/Mastering-TypeScript/dist/masteringTypeScript.js
+++ TypeScript-TechnoCraft/Mastering-TypeScript/dist/masteringTypeScript.js
@@ -72,9 +72,27 @@ function getMostExpensiveProduct(products) {
     }
     return mostExpensiveProduct;
 }
-const products = [
-    { name: "Pen", price: 10 },
-    { name: "Notebook", price: 25 },
-    { name: "Bag", price: 50 }
-];
-console.log(getMostExpensiveProduct(products));
+// const products = [
+//     { name: "Pen", price: 10 },
+//     { name: "Notebook", price: 25 },
+//     { name: "Bag", price: 50 }
+//   ];
+//   console.log(getMostExpensiveProduct(products));  // output will be =>  { name: "Bag", price: 50 }
+// ############ working with enum object ###########
+var Day;
+(function (Day) {
+    Day[Day["Monday"] = 0] = "Monday";
+    Day[Day["Tuesday"] = 1] = "Tuesday";
+    Day[Day["Wednesday"] = 2] = "Wednesday";
+    Day[Day["Thursday"] = 3] = "Thursday";
+    Day[Day["Friday"] = 4] = "Friday";
+    Day[Day["Saturday"] = 5] = "Saturday";
+    Day[Day["Sunday"] = 6] = "Sunday";
+})(Day || (Day = {}));
+function getDayType(day) {
+    if (day === Day.Sunday || day === Day.Saturday) {
+        return `Weekend`;
+    }
+    return "Weekday";
+}
+console.log(getDayType(Day.Monday)); // output will be Weekday
