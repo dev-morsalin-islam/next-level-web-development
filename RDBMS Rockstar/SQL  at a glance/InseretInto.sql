fd551c2e61c4086f523ed742576872a305f71f35
morsalin-islam335 <weshallovercome335@gmail.com>
2025-05-13 22:01:11 +0600

    working with sql insertion

---
+++ RDBMS Rockstar/SQL  at a glance/InseretInto.sql
@@ -0,0 +1,9 @@
+USE LIBRARY;
+
+-- INSERTING DATA
+
+INSERT INTO BOOK(NAME, ID, PRICE, AUTHOR)
+VALUES
+('JavaScript a maro thaba', 23, 550, 'Jhankar Mahbub'),
+('Learn SQL Easily' , 24, 600, 'Morsalin Islam');
+
