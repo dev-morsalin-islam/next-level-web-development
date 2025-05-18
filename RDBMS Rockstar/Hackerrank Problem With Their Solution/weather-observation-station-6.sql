251b94ecad494495fe5495e6bddabbf8c9803312
morsalin-islam335 <weshallovercome335@gmail.com>
2025-05-18 22:33:28 +0600

    weather observation station 6 hackerrank problem 6 solution added

---
+++ RDBMS Rockstar/Hackerrank Problem With Their Solution/weather-observation-station-6.sql
@@ -0,0 +1,10 @@
+
+
+-- problem link: https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+
+SELECT DISTINCT CITY FROM STATION
+    WHERE CITY LIKE 'a%' 
+    OR CITY LIKE 'e%' 
+    OR CITY LIKE 'i%' 
+    OR CITY LIKE 'o%' 
+    OR CITY LIKE 'u%';
