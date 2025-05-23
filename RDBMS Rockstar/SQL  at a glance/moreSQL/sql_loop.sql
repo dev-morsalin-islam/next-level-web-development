56f2f4a60c47bee6e5ba79b7e75fa5a375cb342a
morsalin-islam335 <weshallovercome335@gmail.com>
2025-05-23 21:45:38 +0600

    add prime number finding function

--- RDBMS Rockstar/SQL  at a glance/moreSQL/sql_loop.sql
+++ RDBMS Rockstar/SQL  at a glance/moreSQL/sql_loop.sql
@@ -46,3 +46,68 @@ SELECT * FROM COPY_TABLE;
         WORK
     END LOOP;
 */
+
+
+
+-- function prime numbers from 1 to n optimization version n * sqrt(n) complexity
+
+-- CREATE OR REPLACE FUNCTION PRIME_NUMBERS(PARAM_N INT)
+-- RETURNS TABLE(PRIME_NUMBER INT)
+-- LANGUAGE PLPGSQL
+-- AS
+-- $$
+-- DECLARE
+--     NUM INT;
+--     NUM2 INT;
+--     IS_PRIME BOOLEAN;
+-- BEGIN
+--     FOR NUM IN 2..PARAM_N LOOP
+--         IS_PRIME := TRUE;
+
+--         FOR NUM2 IN 2..CEIL(SQRT(NUM)) LOOP
+--             IF NUM % NUM2 = 0 AND NUM2 <> NUM THEN
+--                 IS_PRIME := FALSE;
+--                 EXIT;
+--             END IF;
+--         END LOOP;
+
+--         IF IS_PRIME THEN
+--             PRIME_NUMBER := NUM;
+--             RETURN NEXT;
+--         END IF;
+--     END LOOP;
+-- END;
+-- $$;
+
+
+-- SELECT * FROM PRIME_NUMBERS(1000);
+
+
+CREATE OR REPLACE FUNCTION PRIME_NUMBERS(N INT)
+RETURNS TABLE(PRIME_NUMBER INT)
+LANGUAGE PLPGSQL
+AS
+$$
+    DECLARE
+        I INT;
+        J INT;
+        IS_PRIME BOOLEAN;
+    BEGIN
+        FOR I IN 2..N LOOP
+
+            IS_PRIME := TRUE;
+            FOR J IN 2..CEIL(SQRT(I)) LOOP
+                IF I % J = 0 THEN
+                    IS_PRIME := FALSE;
+                    EXIT; --  STOP OUTER LOOP
+                END IF;
+            END LOOP;
+            IF IS_PRIME = TRUE THEN
+                PRIME_NUMBER := I;
+                RETURN NEXT;
+            END IF;
+        END LOOP;
+    END;
+$$;
+
+SELECT * FROM PRIME_NUMBERS(100);
