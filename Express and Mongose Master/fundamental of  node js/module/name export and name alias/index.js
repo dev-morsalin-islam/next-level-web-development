d85eebab844afe6cc0bb39cd0fda1e3f7594aa23
morsalin-islam335 <weshallovercome335@gmail.com>
2025-05-29 23:24:07 +0600

    name export and alias

---
+++ Express and Mongose Master/fundamental of  node js/module/name export and name alias/index.js
@@ -0,0 +1,9 @@
+
+
+
+const {add: addition, subtract: subtraction, multiply: multiplication} = require('./calculator.js');
+
+console.log(addition(5, 3)); // 8
+console.log(subtraction(5, 3)); // 2   
+console.log(multiplication(5, 3)); // 15
+
