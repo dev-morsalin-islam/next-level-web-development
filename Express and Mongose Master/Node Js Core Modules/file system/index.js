0ebf7eef416727bf8766f7f696722732e1d7db7e
morsalin-islam335 <weshallovercome335@gmail.com>
2025-06-01 00:25:02 +0600

    working with assynchronoush file reading

--- Express and Mongose Master/Node Js Core Modules/file system/index.js
+++ Express and Mongose Master/Node Js Core Modules/file system/index.js
@@ -4,8 +4,18 @@
 const fs = require("fs");
 
 // synchronously read a file
-const data = fs.readFileSync("./data.txt", "utf-8");
+// const data = fs.readFileSync("./data.txt", "utf-8");
 
-console.log("data:", data);
+// console.log("data:", data);
+
+
+// asynchronously read a file
+
+ fs.readFile("./data.txt", 'utf-8', (error, data) => 
+    {
+        if(error) console.error("Error reading file:", error);
+        else console.log("File data:", data);
+        
+    });
 
 
