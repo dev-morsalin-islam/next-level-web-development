5348488105e77bc0183ad3ed963a460ecbfb32d6
morsalin-islam335 <weshallovercome335@gmail.com>
2025-06-03 00:00:13 +0600

    routing implemented

--- Express and Mongose Master/Node Js Core Modules/to-do-app/server.js
+++ Express and Mongose Master/Node Js Core Modules/to-do-app/server.js
@@ -3,10 +3,34 @@
 const http = require("http");
 
 const server = http.createServer((request, response) =>{
-    response.end("Hello World! This is a simple HTTP server");
+    // console.log("Request: ", request );
+    // console.log("Response: ", response);
+   
+    if(request.url === "/" &&  request.method === "GET") 
+    {
+        response.end("<h1>Welcome to HomePage</h1>")
+    }
+    else if(request.url === "/allTodos" && request.method === "GET")
+    {
+        response.end("<h1> All Todos</h1>")
+    }
+    else if(request.url === "/createTodo" && request.method === "POST")
+    {
+        response.end("<h1> Create Todo</h1>")
+    }
+    else{
+        response.end("<h1>404 Not Found</h1>");
+        console.log(request.url)
+    }
 });
 
 
 server.listen(3000, '127.0.0.1', ()=>{
     console.log("Server is running ✔");
 })
+/*
+    formula:
+    1. Import the http module.
+    2. Create an HTTP server using http.createServer().
+    3. Listen on a specific port and IP address.
+*/
