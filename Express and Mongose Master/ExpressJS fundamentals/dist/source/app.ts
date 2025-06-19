1b05893d01fe229e3573dabb78756b0d9f8072d2
morsalin-islam335 <weshallovercome335@gmail.com>
2025-06-20 02:14:20 +0600

    making a simple express.js application

--- Express and Mongose Master/ExpressJS fundamentals/dist/source/app.ts
+++ Express and Mongose Master/ExpressJS fundamentals/dist/source/app.ts
@@ -2,10 +2,28 @@
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+const router1 = express.Router();
 
+app.use("/router1", router1); // Use the router for a specific path
+
+
+app.use(express.json()); // Middleware to parse JSON bodies from incoming requests
+
+router1.get("/", (request: Request, response: Response) => {
+    response.json({
+        message: "Hello from router1!"
+    })
+})
 app.get("/", (request: Request, response: Response) =>{
-    response.send("Hello world");
+    console.log(request.path);
+    response.send("Learning TypeScript with Express.js");
 })
 // another methods get,post, patch, delete
+
+app.post("/createToDo", (request: Request, response: Response)=>{
+    console.log(request.body);
+   
+    response.send(request.body);
+})
 
 export default app;
--- Express and Mongose Master/ExpressJS fundamentals/out/app.js
+++ Express and Mongose Master/ExpressJS fundamentals/out/app.js
@@ -5,8 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const router1 = express_1.default.Router();
+app.use("/router1", router1); // Use the router for a specific path
+app.use(express_1.default.json()); // Middleware to parse JSON bodies from incoming requests
+router1.get("/", (request, response) => {
+    response.json({
+        message: "Hello from router1!"
+    });
+});
 app.get("/", (request, response) => {
-    response.send("Hello world");
+    console.log(request.path);
+    response.send("Learning TypeScript with Express.js");
 });
 // another methods get,post, patch, delete
+app.post("/createToDo", (request, response) => {
+    console.log(request.body);
+    response.send(request.body);
+});
 exports.default = app;
